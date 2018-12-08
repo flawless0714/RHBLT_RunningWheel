8ab733612ab2638957e72ed24d576a3a6a5cfc41
flawless0714 <flawless0714@gmail.com>
2018-12-08 22:31:43 +0800

    Add  ==Timer 3 Control 0== macro and definition

--- mcu-end/Inc/SI_EFM8LB1_Defs.h
+++ mcu-end/Inc/SI_EFM8LB1_Defs.h
@@ -508,7 +508,18 @@ SI_SBIT (TMR2CN0_T2SPLIT, SFR_TMR2CN0, 3); ///< Timer 2 Split Mode Enable
 SI_SBIT (TMR2CN0_TF2CEN,  SFR_TMR2CN0, 4); ///< Timer 2 Capture Enable             
 SI_SBIT (TMR2CN0_TF2LEN,  SFR_TMR2CN0, 5); ///< Timer 2 Low Byte Interrupt Enable  
 SI_SBIT (TMR2CN0_TF2L,    SFR_TMR2CN0, 6); ///< Timer 2 Low Byte Overflow Flag     
-SI_SBIT (TMR2CN0_TF2H,    SFR_TMR2CN0, 7); ///< Timer 2 High Byte Overflow Flag    
+SI_SBIT (TMR2CN0_TF2H,    SFR_TMR2CN0, 7); ///< Timer 2 High Byte Overflow Flag  
+
+// TMR3CN0 (Timer 3 Control 0)
+#define SFR_TMR3CN0 0x91
+SI_SBIT (TMR3CN0_T3XCLK0, SFR_TMR3CN0, 0); ///< Timer 3 External Clock Select Bit 0
+SI_SBIT (TMR3CN0_T3XCLK1, SFR_TMR3CN0, 1); ///< Timer 3 External Clock Select Bit 1
+SI_SBIT (TMR3CN0_TR3,     SFR_TMR3CN0, 2); ///< Timer 3 Run Control                
+SI_SBIT (TMR3CN0_T23PLIT, SFR_TMR3CN0, 3); ///< Timer 3 Split Mode Enable          
+SI_SBIT (TMR3CN0_TF3CEN,  SFR_TMR3CN0, 4); ///< Timer 3 Capture Enable             
+SI_SBIT (TMR3CN0_TF3LEN,  SFR_TMR3CN0, 5); ///< Timer 3 Low Byte Interrupt Enable  
+SI_SBIT (TMR3CN0_TF3L,    SFR_TMR3CN0, 6); ///< Timer 3 Low Byte Overflow Flag     
+SI_SBIT (TMR3CN0_TF3H,    SFR_TMR3CN0, 7); ///< Timer 3 High Byte Overflow Flag 
 
 // TMR4CN0 (Timer 4 Control 0)
 #define SFR_TMR4CN0 0x98
